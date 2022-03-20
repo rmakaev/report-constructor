657c96e22df982cb447c292a48272bb50bf5d93c
n0vax <alex.aganow@yandex.ru>
2022-03-20 14:58:03 +0300

    Fix table view scroll bar

--- frontend/src/components/ChartView.tsx
+++ frontend/src/components/ChartView.tsx
@@ -1,131 +1,126 @@
-import Chart, { Legend, Series, Label, Export } from "devextreme-react/chart";
+import data from "@/mock/users.json";
+import Chart, { Export, Label, Legend, Series } from "devextreme-react/chart";
+import Form, { GroupItem, SimpleItem } from "devextreme-react/form";
 import "devextreme/dist/css/dx.material.blue.light.css";
 import { useEffect, useMemo, useState } from "react";
-import data from "@/mock/users.json";
-import Form, { SimpleItem, GroupItem } from "devextreme-react/form";
+import styled from "styled-components";
 
 const chartTypes = ["spline", "bar", "line"];
 
+const ChartViewRoot = styled.div({
+  height: "100%",
+  overflow: "auto",
+});
+
 const ChartView = () => {
-    const [keys, setKeys] = useState<string[]>([]);
+  const [keys, setKeys] = useState<string[]>([]);
 
-    const [chartData, setChartData] = useState({
-        argumentField: "name",
-        valueField: "age",
-        tableName: 'table name',
-        rotated: false,
-        showLabel: false,
-        showLegend: true,
-        legendColor: "#c45",
-        chartType: "bar",
+  const [chartData, setChartData] = useState({
+    argumentField: "name",
+    valueField: "age",
+    tableName: "table name",
+    rotated: false,
+    showLabel: false,
+    showLegend: true,
+    legendColor: "#c45",
+    chartType: "bar",
+  });
+
+  const columns = useMemo(() => {
+    const uniqueColumnNames = data.reduce((acc, post) => {
+      for (const key of Object.keys(post)) {
+        if (!(key in acc) && typeof post[key as keyof typeof post] !== "object") {
+          acc[key] = true;
+        }
+      }
+
+      return acc;
+    }, {} as Record<string, boolean>);
+
+    return Object.keys(uniqueColumnNames).map((columnName) => {
+      return columnName;
     });
+  }, []);
 
-    const columns = useMemo(() => {
-        const uniqueColumnNames = data.reduce((acc, post) => {
-            for (const key of Object.keys(post)) {
-                if (
-                    !(key in acc) &&
-                    typeof post[key as keyof typeof post] !== "object"
-                ) {
-                    acc[key] = true;
-                }
-            }
+  const setField = ({ dataField, value }: any) => {
+    setChartData((prev) => ({ ...prev, [dataField]: value }));
+  };
 
-            return acc;
-        }, {} as Record<string, boolean>);
+  useEffect(() => {
+    columns && setKeys(columns);
+  }, [columns]);
 
-        return Object.keys(uniqueColumnNames).map((columnName) => {
-            return columnName;
-        });
-    }, []);
+  return (
+    <ChartViewRoot>
+      <Form
+        onFieldDataChanged={setField}
+        formData={chartData}
+        labelMode="floating"
+        labelLocation="left"
+        style={{ padding: "9px" }}
+      >
+        <GroupItem cssClass="first-group" colCount={6}>
+          <GroupItem colSpan={2}>
+            <SimpleItem
+              dataField="argumentField"
+              editorType="dxSelectBox"
+              editorOptions={{
+                items: keys.filter((x) => x !== chartData.valueField),
+              }}
+            />
+            <SimpleItem
+              dataField="valueField"
+              editorType="dxSelectBox"
+              editorOptions={{
+                items: keys.filter((x) => x !== chartData.argumentField),
+              }}
+            />
+          </GroupItem>
 
-    const setField = ({ dataField, value }: any) => {
-        setChartData((prev) => ({ ...prev, [dataField]: value }));
-    };
+          <GroupItem colSpan={3}>
+            <SimpleItem dataField="legendColor" editorType="dxColorBox" />
+            <SimpleItem
+              colSpan={2}
+              dataField="chartType"
+              editorType="dxSelectBox"
+              editorOptions={{ items: chartTypes }}
+            />
+          </GroupItem>
+          <GroupItem colSpan={1}>
+            <SimpleItem dataField="showLabel" editorType="dxCheckBox" />
+            <SimpleItem dataField="showLegend" editorType="dxCheckBox" />
 
-    useEffect(() => {
-        columns && setKeys(columns);
-    }, [columns]);
+            <SimpleItem dataField="rotated" editorType="dxCheckBox" />
+          </GroupItem>
+        </GroupItem>
+        <GroupItem colCount={6}>
+          <SimpleItem colSpan={6} dataField="tableName" />
+        </GroupItem>
+      </Form>
 
-    return (
-        <>
-            <Form
-                onFieldDataChanged={setField}
-                formData={chartData}
-                labelMode="floating"
-                labelLocation="left"
-                style={{ padding: '9px' }}
-            >
-                <GroupItem cssClass="first-group" colCount={6}>
-                    <GroupItem colSpan={2}>
-                        <SimpleItem
-                            dataField="argumentField"
-                            editorType="dxSelectBox"
-                            editorOptions={{
-                                items: keys.filter((x) => x !== chartData.valueField),
-                            }}
-                        />
-                        <SimpleItem
-                            dataField="valueField"
-                            editorType="dxSelectBox"
-                            editorOptions={{
-                                items: keys.filter((x) => x !== chartData.argumentField),
-                            }}
-                        />
+      <Chart
+        style={{ padding: "9px" }}
+        title={chartData.tableName}
+        dataSource={data}
+        id="chart"
+        rotated={chartData.rotated}
+      >
+        <Series
+          valueField={chartData.valueField}
+          argumentField={chartData.argumentField}
+          name="My oranges"
+          type={chartData.chartType}
+          color={chartData.legendColor}
+        >
+          <Label visible={chartData.showLabel} backgroundColor="#c18e92" />
+        </Series>
 
-                    </GroupItem>
+        <Legend visible={chartData.showLegend} />
 
-                    <GroupItem colSpan={3}>
-                        <SimpleItem dataField="legendColor" editorType="dxColorBox" />
-                        <SimpleItem
-                            colSpan={2}
-                            dataField="chartType"
-                            editorType="dxSelectBox"
-                            editorOptions={{ items: chartTypes }}
-                        />
-
-                    </GroupItem>
-                    <GroupItem colSpan={1}>
-                        <SimpleItem dataField="showLabel" editorType="dxCheckBox" />
-                        <SimpleItem dataField="showLegend" editorType="dxCheckBox" />
-
-                        <SimpleItem dataField="rotated" editorType="dxCheckBox" />
-                    </GroupItem>
-                </GroupItem>
-                <GroupItem colCount={6}>
-
-                    <SimpleItem colSpan={6}
-                        dataField="tableName"
-                    />
-
-
-                </GroupItem>
-
-            </Form>
-
-            <Chart
-                style={{ padding: '9px' }}
-                title={chartData.tableName}
-                dataSource={data}
-                id="chart"
-                rotated={chartData.rotated}
-            >
-                <Series
-                    valueField={chartData.valueField}
-                    argumentField={chartData.argumentField}
-                    name="My oranges"
-                    type={chartData.chartType}
-                    color={chartData.legendColor}
-                >
-                    <Label visible={chartData.showLabel} backgroundColor="#c18e92" />
-                </Series>
-
-                <Legend visible={chartData.showLegend} />
-
-                <Export enabled={true} />
-            </Chart>
-        </>
-    );
+        <Export enabled={true} />
+      </Chart>
+    </ChartViewRoot>
+  );
 };
 
 export default ChartView;
--- frontend/src/pages/ChartViewPage.tsx
+++ frontend/src/pages/ChartViewPage.tsx
@@ -1,14 +1,20 @@
-import "devextreme/dist/css/dx.material.blue.light.css";
-
-import Toolbar from "@/components/Toolbar";
 import ChartView from "@/components/ChartView";
+import Toolbar from "@/components/Toolbar";
+import "devextreme/dist/css/dx.material.blue.light.css";
+import styled from "styled-components";
+
+const TableViewPageRoot = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+});
 
 const TableViewPage = () => {
   return (
-    <>
+    <TableViewPageRoot>
       <Toolbar />
       <ChartView />
-    </>
+    </TableViewPageRoot>
   );
 };
 
